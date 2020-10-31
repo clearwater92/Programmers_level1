72c294effd6c0351d7faf98e1db8d1419ad64a74
clearwater <clearwater92@naver.com>
2020-11-01 00:24:19 +0900

    failure rate

--- Lv1_failure-rate.py
+++ Lv1_failure-rate.py
@@ -1,31 +1,13 @@
-from collections import Counter
-
 def solution(N, stages):
-
-    # 해당 스테이지를 플레이하고 있는 사람 수
-    stage = Counter(stages)
-
-    # 실패율
-    failure = [0]*N
+    result = {}
     num = len(stages)
-    for i in range(1, N+1) :
-        if stage[i] != 0 :
-            failure[i-1] = stage[i]/num
-            num -= stage[i]
 
-    answer = []
-    num = len(failure)
-    for i, item in enumerate(failure):
-        m = item
-        idx = i
-        for j, _item in enumerate(failure[::-1]):
-            if _item >= m:
-                m = _item
-                idx = num -1-j
-        answer.append(idx+1)
-        failure[idx] = -1
+    for stage in range(1, N+1):
+        if num != 0:
+            count = stages.count(stage)
+            result[stage] = count / num
+            num -= count
+        else:
+            result[stage] = 0
 
-    return answer
-
-print(solution(5,[2,1,2,6,2,4,3,3]))
-print(solution(4,[4,4,4,4,4]))
+    return sorted(result, key=lambda x : result[x], reverse=True)
